d1a901ac3279cb74d0791d01df82c71881dfcbe5
dbarania <01178210@pw.edu.pl>
2023-04-11 15:56:28 +0200

    Testing  framework is now ready renamed some classes

---
+++ include/counters_list.h
@@ -0,0 +1,14 @@
+ //
+// Created by damia on 06.04.2023.
+//
+
+#ifndef PROI_23L_101_APTEKA_COUNTERS_LIST_H
+#define PROI_23L_101_APTEKA_COUNTERS_LIST_H
+
+
+class CountersList {
+
+};
+
+
+#endif //PROI_23L_101_APTEKA_COUNTERS_LIST_H
