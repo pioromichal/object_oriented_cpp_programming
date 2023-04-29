eff959efd304b9a7cb54a675adbaff941e367f65
dbarania <01178210@pw.edu.pl>
2023-04-29 11:34:19 +0200

    Added basic draft for the inventory class with possible methods

--- include/inventory.h
+++ include/inventory.h
@@ -1,14 +1,29 @@
-//
-// Created by damia on 06.04.2023.
-//
-
 #ifndef PROI_23L_101_APTEKA_INVENTORY_H
 #define PROI_23L_101_APTEKA_INVENTORY_H
-
+#include "map"
+#include "set"
+#include "memory"
+#include "medicine.h"
+enum Effect{
+    Cough,
+    Headache
+};
 
 class Inventory {
-    //Wykorzystać mapę, sety i inne pierdoły
+private:
+    //TODO na dodać komparator medicine do setu
+    std::map<Effect,std::unique_ptr<std::set<std::shared_ptr<Medicine>> >> inventory;
+    //Jeśli lek byłby tworzony przy pomocy pliku i fileManagera
+    void addMedicine(std::unique_ptr<Medicine>&);
+public:
+    Inventory();
+    std::shared_ptr<Medicine> findSubstitute(const std::shared_ptr<Medicine>& medicine) const;
+    void pickMedicine(const std::shared_ptr<Medicine>& medicine);
+    bool isMedicineInMagazine(const std::shared_ptr<Medicine>& medicine) const;
+    std::shared_ptr<Medicine> findRandomMedicine();
+    std::shared_ptr<Medicine> findRandomMedicine(Effect effect);
 };
+
 
 
 #endif //PROI_23L_101_APTEKA_INVENTORY_H
--- src/inventory.cpp
+++ src/inventory.cpp
@@ -3,3 +3,29 @@
 //
 
 #include "../include/inventory.h"
+
+Inventory::Inventory() = default;
+
+void Inventory::addMedicine(std::unique_ptr<Medicine> &) {
+
+}
+
+std::shared_ptr<Medicine> Inventory::findSubstitute(const std::shared_ptr<Medicine> &medicine) const {
+    return std::shared_ptr<Medicine>();
+}
+
+void Inventory::pickMedicine(const std::shared_ptr<Medicine> &medicine) {
+
+}
+
+bool Inventory::isMedicineInMagazine(const std::shared_ptr<Medicine> &medicine) const {
+    return false;
+}
+
+std::shared_ptr<Medicine> Inventory::findRandomMedicine() {
+    return std::shared_ptr<Medicine>();
+}
+
+std::shared_ptr<Medicine> Inventory::findRandomMedicine(Effect effect) {
+    return std::shared_ptr<Medicine>();
+}
