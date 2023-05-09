1ab0573f121500a183d79b638b348dd5d382b234
dbarania <01178210@pw.edu.pl>
2023-05-09 23:59:02 +0200

    Counters and CounterList

--- include/counters_list.h
+++ include/counters_list.h
@@ -4,11 +4,35 @@
 
 #ifndef PROI_23L_101_APTEKA_COUNTERS_LIST_H
 #define PROI_23L_101_APTEKA_COUNTERS_LIST_H
-
-
+#include <set>
+#include <memory>
+#include "counter.h"
 class CountersList {
+private:
+    std::set<std::unique_ptr<Counter>> setCounters;
 
-};
+    unsigned char openedCounters;
+
+    void addCounter(bool openCounter);
+public:
+    CountersList(unsigned numberOfCounters, unsigned char openedCounters);
+
+    std::unique_ptr<Counter>& findLongestWorking();
+
+    void openCounter(unsigned id);
+
+    void closeCounter(unsigned id);
+
+    unsigned char getOpenedCounters() const;
+
+    std::unique_ptr<Counter>& getCounter(unsigned id);
+
+    void nextTurn();
+
+#ifdef TESTING_ENV
+    const std::set<std::unique_ptr<Counter>>& getSet() const;
+#endif
+ };
 
 
 #endif //PROI_23L_101_APTEKA_COUNTERS_LIST_H
--- src/counters_list.cpp
+++ src/counters_list.cpp
@@ -1,5 +1,80 @@
-//
-// Created by damia on 06.04.2023.
-//
+#include "../include/counters_list.h"
+#include "../include/counter.h"
+#include <algorithm>
+#include <stdexcept>
 
-#include "../include/windows.h"
+CountersList::CountersList(unsigned int numberOfCounters, unsigned char openedCounters):openedCounters(openedCounters) {
+    if(numberOfCounters<openedCounters){
+        throw std::invalid_argument("You cannot open more counters than there is in the pharmacy");
+    }
+    for(int i=0;i<numberOfCounters;i++){
+        if(i<openedCounters){
+            addCounter(true);
+        }
+        else{
+            addCounter(false);
+        }
+    }
+}
+
+std::unique_ptr<Counter>& CountersList::findLongestWorking() {
+    auto it = std::max_element(setCounters.begin(),setCounters.end(),&Counter::compareTimeWorking);
+    return const_cast<std::unique_ptr<Counter> &>(*it);
+}
+
+unsigned char CountersList::getOpenedCounters() const {
+    return openedCounters;
+}
+
+void CountersList::addCounter(bool openCounter) {
+    std::unique_ptr<Counter> ptr = std::make_unique<Counter>(openCounter);
+    setCounters.insert(std::move(ptr));
+}
+
+void CountersList::nextTurn() {
+    for(auto &counter:setCounters){
+        if(counter->isOpened()){
+            (*counter)++;
+        }
+    }
+}
+#ifdef TESTING_ENV
+const std::set<std::unique_ptr<Counter>> &CountersList::getSet() const {
+    return setCounters;
+}
+#endif
+
+void CountersList::openCounter(unsigned int id) {
+    if(id>setCounters.size()){
+        throw std::invalid_argument("There is no counter of such id");
+    }
+    for(auto &counter:setCounters){
+        if(counter->getId()==id){
+            counter->openCounter();
+            return;
+        }
+    }
+}
+
+void CountersList::closeCounter(unsigned int id) {
+    if(id>setCounters.size()){
+        throw std::invalid_argument("There is no counter of such id");
+    }
+    for(auto &counter:setCounters){
+        if(counter->getId()==id){
+            counter->closeCounter();
+            return;
+        }
+    }
+}
+
+std::unique_ptr<Counter> &CountersList::getCounter(unsigned int id) {
+    if(id>setCounters.size()){
+        throw std::invalid_argument("There is no counter of such id");
+    }
+    for(auto &counter:setCounters){
+        if(counter->getId()==id){
+            return const_cast<std::unique_ptr<Counter> &>(counter);
+        }
+    }
+}
