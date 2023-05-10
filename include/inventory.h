cf8d09cb46e033d14116093c0e8545bf23a515c9
dbarania <01178210@pw.edu.pl>
2023-05-10 18:55:13 +0200

    Some of the inventory methods implemented

--- include/inventory.h
+++ include/inventory.h
@@ -1,27 +1,22 @@
 #ifndef PROI_23L_101_APTEKA_INVENTORY_H
 #define PROI_23L_101_APTEKA_INVENTORY_H
-#include "map"
-#include "set"
-#include "memory"
+#include <map>
+#include <set>
+#include <memory>
 #include "medicine.h"
-enum Effect{
-    Cough,
-    Headache
-};
 
 class Inventory {
 private:
-    //TODO na dodać komparator medicine do setu
-    std::map<Effect,std::unique_ptr<std::set<std::shared_ptr<Medicine>> >> inventory;
+    std::map<Affliction,std::unique_ptr<std::set<std::shared_ptr<Medicine>> >> inventory;
     //Jeśli lek byłby tworzony przy pomocy pliku i fileManagera
-    void addMedicine(std::unique_ptr<Medicine>&);
+    void addMedicine(std::unique_ptr<Medicine>);
 public:
     Inventory();
-    std::shared_ptr<Medicine> findSubstitute(const std::shared_ptr<Medicine>& medicine) const;
+    std::shared_ptr<Medicine> findSubstitute(const std::shared_ptr<Medicine>& medicine);
     void pickMedicine(const std::shared_ptr<Medicine>& medicine);
     bool isMedicineInMagazine(const std::shared_ptr<Medicine>& medicine) const;
     std::shared_ptr<Medicine> findRandomMedicine();
-    std::shared_ptr<Medicine> findRandomMedicine(Effect effect);
+    std::shared_ptr<Medicine> findRandomMedicine(Affliction affliction);
 };
 
 
--- src/inventory.cpp
+++ src/inventory.cpp
@@ -3,29 +3,48 @@
 //
 
 #include "../include/inventory.h"
+#include <exception>
+#include <random>
 
-Inventory::Inventory() = default;
-
-void Inventory::addMedicine(std::unique_ptr<Medicine> &) {
-
+Inventory::Inventory(){
+    char numberOfAfflictions = (char)Affliction::Count;
+    for(char i = 0;i<numberOfAfflictions;i++){
+        inventory[static_cast<Affliction>(i)] = std::make_unique<std::set<std::shared_ptr<Medicine>>>();
+    }
 }
 
-std::shared_ptr<Medicine> Inventory::findSubstitute(const std::shared_ptr<Medicine> &medicine) const {
-    return std::shared_ptr<Medicine>();
+void Inventory::addMedicine(std::unique_ptr<Medicine> medicine) {
+    Affliction affliction = medicine->getAfflication();
+    inventory[affliction]->insert(std::move(medicine));
+}
+
+std::shared_ptr<Medicine> Inventory::findSubstitute(const std::shared_ptr<Medicine> &medicine){
+    Affliction affliction = medicine->getAfflication();
+    for(auto medicinePtr:*inventory[affliction]){
+        if(medicinePtr->getBasePrice()<medicine->getBasePrice() && this->isMedicineInMagazine(medicinePtr)){
+            return medicinePtr;
+        }
+    }
+    throw std::exception();
 }
 
 void Inventory::pickMedicine(const std::shared_ptr<Medicine> &medicine) {
-
+    medicine->operator--();
 }
 
 bool Inventory::isMedicineInMagazine(const std::shared_ptr<Medicine> &medicine) const {
-    return false;
+    return medicine->getAmountInPharmacy()>0;
 }
 
 std::shared_ptr<Medicine> Inventory::findRandomMedicine() {
-    return std::shared_ptr<Medicine>();
+//    return std::shared_ptr<Medicine>();
+    std::default_random_engine generator;
+    std::uniform_int_distribution<int> distributionAffliction(0,(int)Affliction::Count);
+    Affliction affliction = static_cast<Affliction>(distributionAffliction(generator));
+    std::uniform_int_distribution<int> distributionMedicine(0,inventory[affliction]->size());
+    
 }
 
-std::shared_ptr<Medicine> Inventory::findRandomMedicine(Effect effect) {
+std::shared_ptr<Medicine> Inventory::findRandomMedicine(Affliction affliction) {
     return std::shared_ptr<Medicine>();
 }
