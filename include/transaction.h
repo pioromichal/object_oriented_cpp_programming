61c1004a1153fddcccec8c4c373748d777a98689
Michał Pióro <pioromichal@outlook.com>
2023-05-19 22:20:55 +0200

    Impemented working version of transaction with some tests

--- include/transaction.h
+++ include/transaction.h
@@ -1,13 +1,35 @@
-//
-// Created by damia on 06.04.2023.
-//
-
 #ifndef PROI_23L_101_APTEKA_TRANSACTION_H
 #define PROI_23L_101_APTEKA_TRANSACTION_H
 
+#include "client.h"
+#include "counter.h"
+#include "inventory.h"
+#include <memory>
 
 class Transaction {
+	private:
+		Inventory& pharmacyIneventory;
+		std::unique_ptr<Client>& supportedClientPtr;
+		std::unique_ptr<Counter>& currentCounterPtr;
+		unsigned remainingTime;
+		Price nettoPrice;
+		Price bruttoPrice;
+		Price taxPrice;
+		
+		void calculatePrices();
+		void setStartingRemainingTime();
+		void manageShoppingList();
+		void randomlyFindSubstitute(ShoppingItem& medicineOnList);
 
+	public:
+		Transaction(Inventory& pharmacyIneventory, std::unique_ptr<Client>& supportedClientPtr, std::unique_ptr<Counter>& currentCounterPtr);
+		bool isFinished() const;
+		Transaction& operator--();
+		unsigned getRemainingTime() const;
+		Price getNettoPrice() const;
+		Price getBruttoPrice() const;
+		Price getTaxPrice() const;
+		~Transaction();
 };
 
 
--- src/transaction.cpp
+++ src/transaction.cpp
@@ -1,5 +1,97 @@
-//
-// Created by damia on 06.04.2023.
-//
-
 #include "../include/transaction.h"
+
+#include <chrono>
+#include <random>
+
+Transaction::Transaction(Inventory& pharmacyIneventory, std::unique_ptr<Client>& supportedClientPtr, std::unique_ptr<Counter>& currentCounterPtr)
+	: pharmacyIneventory(pharmacyIneventory), supportedClientPtr(supportedClientPtr), currentCounterPtr(currentCounterPtr), remainingTime(0), nettoPrice(Price()), bruttoPrice(Price()), taxPrice(Price()) {
+	currentCounterPtr->setIsOccupied(true);
+
+	setStartingRemainingTime();
+
+	manageShoppingList();
+	
+	calculatePrices();
+
+	//logger - podejœcie klienta
+}
+
+void Transaction::calculatePrices() {
+	this->nettoPrice = supportedClientPtr->calculateNettoPrice();
+	this->taxPrice = supportedClientPtr->calculateTax();
+	this->bruttoPrice = supportedClientPtr->calculateBruttoPrice();
+}
+
+void Transaction::setStartingRemainingTime() {
+	this->remainingTime = supportedClientPtr->getShoppingList().getListSize() / 3;
+	if (supportedClientPtr->getShoppingList().getListSize() % 3 != 0) {
+		this->remainingTime++;
+	}
+}
+
+void Transaction::manageShoppingList() {
+	for (ShoppingItem& medicineOnList : supportedClientPtr->getMedicinesList()) {
+		this->randomlyFindSubstitute(medicineOnList);
+
+		if (pharmacyIneventory.isMedicineInMagazine(medicineOnList.getMedicinePtr(), medicineOnList.getNumberOfMedicines())) {
+			pharmacyIneventory.pickMedicine(medicineOnList.getMedicinePtr(), medicineOnList.getNumberOfMedicines());
+		} else {
+			unsigned soldAmount = pharmacyIneventory.howManyInMagazine(medicineOnList.getMedicinePtr());
+			unsigned leftAmount = medicineOnList.getNumberOfMedicines() - soldAmount;
+
+			pharmacyIneventory.pickMedicine(medicineOnList.getMedicinePtr(), soldAmount);
+			supportedClientPtr->changeMedcineAmount(medicineOnList.getMedicinePtr(), soldAmount);
+
+			std::shared_ptr<Medicine> substitutePtr = pharmacyIneventory.findSubstitute(medicineOnList.getMedicinePtr());
+			if (substitutePtr == nullptr) {
+				//logger - nie sprzedano
+			} else {
+				//logger - sprzedano dowolne zamienniki
+				pharmacyIneventory.pickMedicine(substitutePtr, leftAmount);
+				supportedClientPtr->addMedicineToList(substitutePtr, leftAmount);
+			}
+		}
+	}
+}
+
+void Transaction::randomlyFindSubstitute(ShoppingItem& medicineOnList) {
+	unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+	std::mt19937 generator(seed);
+	std::bernoulli_distribution distribution(supportedClientPtr->getProbabilityOfActions());
+	bool findSubs = distribution(generator);
+	if (findSubs) {
+		std::shared_ptr<Medicine> substitutePtr = pharmacyIneventory.findSubstitute(medicineOnList.getMedicinePtr());
+		supportedClientPtr->replaceMedicineOnList(medicineOnList.getMedicinePtr(), substitutePtr);
+		medicineOnList.setMedicinePtr(substitutePtr);
+		//logger - znaleziono zamienniki
+	}
+}
+
+unsigned Transaction::getRemainingTime() const {
+	return remainingTime;
+}
+
+Price Transaction::getNettoPrice() const {
+	return nettoPrice;
+}
+
+Price Transaction::getBruttoPrice() const {
+	return bruttoPrice;
+}
+
+Price Transaction::getTaxPrice() const {
+	return taxPrice;
+}
+
+bool Transaction::isFinished() const {
+	return remainingTime == 0;
+}
+
+Transaction& Transaction::operator--() {
+	remainingTime -= 1;
+	return *this;
+}
+
+Transaction::~Transaction() {
+	currentCounterPtr->setIsOccupied(false);
+}
