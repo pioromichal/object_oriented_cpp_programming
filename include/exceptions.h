e2ea7b48f8ff2f271bbeef49b9d378c77b4755b4
Michał Pióro <pioromichal@outlook.com>
2023-05-21 21:40:33 +0200

    Added some functionality of shopping list and implemented exceptions

---
+++ include/exceptions.h
@@ -0,0 +1,14 @@
+#ifndef EXCEPTIONS_H
+#define EXCEPTIONS_H
+
+#include <stdexcept>
+
+namespace Exceptions {
+    class ProbabilityOutOfRange : public std::invalid_argument {
+    public: ProbabilityOutOfRange(const float probability);
+    };
+    class MedicineDoesntExistOnList :public std::invalid_argument {
+    public: MedicineDoesntExistOnList(const std::string nameOfMedicine);
+    };
+}
+#endif
