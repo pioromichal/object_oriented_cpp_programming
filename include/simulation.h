9c7feddbbf0a323d74893dd11ad69bafc4969f33
dbarania <baraniak.damian@gmail.com>
2023-05-26 21:41:00 +0200

    Some updates to creating simulation from JSON file

--- include/simulation.h
+++ include/simulation.h
@@ -1,17 +1,32 @@
-//
-// Created by damia on 06.04.2023.
-//
-
 #ifndef PROI_23L_101_APTEKA_SIMULATION_H
 #define PROI_23L_101_APTEKA_SIMULATION_H
+
+#include <vector>
+#include <string>
+#include <fstream>
 #include "pharmacy.h"
 
-class Simiulation {
+using std::ifstream;
+struct Names {
+    std::vector<std::string> firstNames;
+    int fLength = 0;
+    std::vector<std::string> lastNames;
+    int lLength = 0;
+};
+struct Medicines{
+    std::vector<std::string> medicineNames;
+    int length = 0;
+};
+
+class Simulation {
 private:
     int numberOfTurns;
     Pharmacy pharmacy;
+    Names namesData;
+    Medicines medicineData;
 public:
-    Simiulation();
+    Simulation(int nTurns, int nMedicines, int nCounters, int nOpenedCounters, int nStartingClients, ifstream &firstNames,
+               ifstream &lastNames, ifstream &medicineNames);
 };
 
 
