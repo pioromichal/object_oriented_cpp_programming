48646a71be27accf47db7da0f1173c5512e68d79
Michał Pióro <pioromichal@outlook.com>
2023-05-27 15:33:50 +0200

    Implemented iteration in simulation with needed methods in simulation and pharmacy

--- src/pharmacy.cpp
+++ src/pharmacy.cpp
@@ -102,3 +102,58 @@ float Pharmacy::randomArguability() {
 ShoppingList Pharmacy::randomShoppingList() {
     return ShoppingList();
 }
+
+Inventory& Pharmacy::getInventory()
+{
+	return inventory;
+}
+
+unsigned Pharmacy::getNumOfCounters() const
+{
+	return counters.getAllCounters();
+}
+
+unsigned Pharmacy::getNumOfOpenCounters() const {
+	return counters.getOpenedCounters();
+}
+
+std::unique_ptr<Counter>& Pharmacy::getCounter(unsigned id) {
+	return counters.getCounter(id);
+}
+
+std::unique_ptr<Counter>& Pharmacy::findLongestWorkingCounter() {
+	return counters.findLongestWorking();
+}
+
+void Pharmacy::openCounter(unsigned id) {
+	counters.openCounter(id);
+}
+
+void Pharmacy::incrementCounters() {
+	counters.nextTurn();
+}
+
+void Pharmacy::pushBusinessClient(std::string name, std::string surname, ShoppingList& shoppingList, float probabilityOfActions) {
+	queue.pushBusinessClient(name, surname, shoppingList, probabilityOfActions);
+}
+
+void Pharmacy::pushIndividualClient(std::string name, std::string surname, ShoppingList& shoppingList, float probabilityOfActions) {
+	queue.pushIndividualClient(name, surname, shoppingList, probabilityOfActions);
+}
+
+std::shared_ptr<Medicine> Pharmacy::findRandomMedicine() {
+	return inventory.findRandomMedicine();
+}
+
+CountersList& Pharmacy::getCountersList() {
+	return counters;
+}
+
+std::unique_ptr<Counter>& Pharmacy::getOpenCounter()
+{
+	return counters.getOpenCounter();
+}
+
+std::unique_ptr<Client> Pharmacy::popClient() {
+	return queue.popClient();
+}
